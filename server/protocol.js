b736cda7b52fb274723f7f4e3663a5957f9f763b
Isaranu Janthong <isaranujanthong@yahoo.com>
2020-04-06 13:06:27 +0700

    Update protocol.js

--- server/protocol.js
+++ server/protocol.js
@@ -1,1 +1,21 @@
+var net = require('net');
+var port = 5000;
 
+var server = net.createServer(function(client){
+    console.log('client connect');
+
+    client.on('data', function(raw){
+        console.log(raw);
+        var log = raw.toString('ascii');
+        console.log(log);
+    });
+    client.on('end', function(){
+        console.log('client disconnected');
+    });
+    client.write('OK\r\n');
+    client.pipe(client);
+})
+
+server.listen(port, function(){
+    console.log('server is listening ');
+})
